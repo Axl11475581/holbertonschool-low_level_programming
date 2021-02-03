e6838e9b90dc453e373384beed5d304fb7023625
Axl11475581 <slopezviteri@gmail.com>
2021-02-03 23:12:13 +0000

    Attemp 1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
-
 /**
- * Main - declaration of variables, functions and if functions.
- * Return - always 0.
+ * main - declaration of variables, functions and if functions.
+ *Description: Print different types
+ * Return: always 0.
  **/
-int main(void) {
-	int i = 0;
-	char c;
-	long int li;
-	long long int lli;
-	float f;
-	
-	printf("Size of a char : %ld byte	
+int main(void)
+{
+	int intType;
+	char charType;
+	long int longintType;
+	long long int llintType;
+	float floatType;
+
+	printf("Size of a char : %zu bytes\n", sizeof(charType));
+	printf("Size of a int : %zu bytes\n", sizeof(intType));
+	printf("Size of float : %zu bytes\n", sizeof(floatType));
+	printf("Size of long int: %zu bytes\n", sizeof(longintType));
+	printf("Size of long long int: %zu bytes\n", sizeof(llintType));
+	return (0);
+}
+
