83a2246391a09e6149b6c742de839456adfebb6e
Axl11475581 <slopezviteri@gmail.com>
2021-02-03 21:53:32 +0000

    Attemp 1

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - declaration of char
+ * Description: Print the string to the console.
+ * Return: returns the value 0.
+ */
+int main(void)
+{
+	char str[100] = "with proper grammar, but the outcome is a piece of art,";
+	printf("%s \n", str);
+	return (0);
+}
