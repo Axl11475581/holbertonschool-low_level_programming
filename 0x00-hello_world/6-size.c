5921a22496b903b18b1404065d0f3c7ffa35429e
Axl11475581 <slopezviteri@gmail.com>
2021-02-03 21:10:51 +0000

    Quick fix

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Main - declaration of variables, functions and if functions.
+ * Return - always 0.
+ **/
+int main(void) {
+	int i = 0;
+	char c;
+	long int li;
+	long long int lli;
+	float f;
+	
+	printf("Size of a char : %ld byte	
