7c612b6c6ad32f8aa05538cfbee781b71e1ea7bd
Axl11475581 <slopezviteri@gmail.com>
2021-02-03 21:20:59 +0000

    Attemp 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-/*
-* Main - display the string.
-* Return - the function value returns 0.
-*/
+/**
+ * main - display the string.0
+ * Description: Prints the string to the console.
+ * Return: the function value returns 0
+ */
 int main(void)
 {
 	char str[50];
 
-	strcpy(str, "\"Programming is like building a multilingual puzzle\n");
+	strcpy(str, "\"Programming is like building a multilingual puzzle");
 	puts(str);
 	return (0);
 }
