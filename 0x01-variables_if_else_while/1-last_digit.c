cd21b389715d0198e7ec2da6602c0f9866c9f7ab
Axl11475581 <slopezviteri@gmail.com>
2021-02-04 16:42:51 +0000

    Attemp 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - number randomizer with if function
+ * Description: Print the last digit of the number stored in the n variable.
+ * Return: always 0.
+ */
+int main(void)
+{
+	return (0);
+}
