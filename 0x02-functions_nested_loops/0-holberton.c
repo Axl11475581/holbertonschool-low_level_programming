f82cb527121528c7fb0bde9d617c90d23fc7d43e
Axl11475581 <slopezviteri@gmail.com>
2021-02-09 13:51:12 +0000

    Fix 1

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - type void
+ * Description: function that prints holberton.
+ * Return: always cero.
+ */
+int main(void)
+{
+	char c = "Holberton"
+
+	printf("%d\n", c);
+	return (0);
+}
